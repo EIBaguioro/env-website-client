05b98c21eec8c37ec1dfd1726b54d4846f5c0010
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-01-18 22:25:15 +0800

    create topnav component

--- src/components/topnav/index.js
+++ src/components/topnav/index.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+
+import Link from 'next/link';
+
 function Topnav() {
+  const [isLoggedIn, setIsLoggedin] = useState(true);
 
-    return <h1>Topnav</h1>
-
+  return (
+    <nav className='topnav'>
+      <p className='logo'>ENV.</p>
+      <ul className='navlist'>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        {!isLoggedIn ? (
+          <>
+            <li>
+              <Link href='/auth/login'>Login</Link>
+            </li>
+            <li>
+              <Link href='/auth/signup'>Sign up</Link>
+            </li>{' '}
+          </>
+        ) : (
+          <li>HI USER</li>
+        )}
+      </ul>
+    </nav>
+  );
 }
 
 export default Topnav;
