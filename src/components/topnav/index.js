d1906cfd2035dc83e5cc6db42a98fcdd7192c4cf
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-01-18 23:52:09 +0800

    style topnav

--- src/components/topnav/index.js
+++ src/components/topnav/index.js
@@ -2,29 +2,41 @@ import { useState } from 'react';
 
 import Link from 'next/link';
 
+import styles from './topnav.module.css';
+
 function Topnav() {
   const [isLoggedIn, setIsLoggedin] = useState(false);
 
   return (
-    <nav className='topnav'>
-      <p className='logo'>ENV.</p>
-      <ul className='navlist'>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        {!isLoggedIn ? (
-          <>
-            <li>
-              <Link href='/auth/login'>Login</Link>
-            </li>
-            <li>
-              <Link href='/auth/signup'>Sign up</Link>
-            </li>{' '}
-          </>
-        ) : (
-          <li>HI USER</li>
-        )}
-      </ul>
+    <nav id={styles['topnav']}>
+      <div className={`container flex ${styles.topnav}`}>
+        <p className={styles.logo}>ENV.</p>
+        <ul className={styles.navlist}>
+          <li>
+            <Link
+              href='/'
+            >
+              Home
+            </Link>
+          </li>
+          {!isLoggedIn ? (
+            <>
+              <li>
+                <Link
+                  href='/auth/login'
+                >
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link href='/auth/signup'>Sign up</Link>
+              </li>{' '}
+            </>
+          ) : (
+            <li className={styles.greeting}>HI USER</li>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 }
