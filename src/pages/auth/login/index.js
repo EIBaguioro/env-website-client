1caecf0e6d95fe48eabce2773ed63af565fee4d8
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-01-20 17:20:47 +0800

    create signup and login pages

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -1,5 +1,27 @@
+import Link from 'next/link';
+import styles from './login.module.css';
+
 function Login() {
-    return <h1>Login</h1>
+    return (
+      <div id={styles['login']}>
+        <div className={`container ${styles.login}`}>
+          <form>
+            <h3>Login</h3>
+            <input type='email' name='email' id='email' placeholder='email' />
+            <input
+              type='password'
+              name='password'
+              id='password'
+              placeholder='password'
+            />
+            <button className='btn'>Login</button>
+            <p>
+              You don't have an account yet. <Link href="/auth/signup">Sign up</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    );
 }
 
 export default Login;
--- src/pages/auth/signup/index.js
+++ src/pages/auth/signup/index.js
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+import styles from './signup.module.css';
+
 function Signup() {
-  return <h1>Signup</h1>;
+  return (
+    <div id={styles['signup']}>
+      <div className={`container ${styles.signup}`}>
+        <form>
+          <h3>Sign Up</h3>
+          <input type="text" name="name" id="name" placeholder='name'/>
+          <input type='email' name='email' id='email' placeholder='email' />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            placeholder='password'
+          />
+          <button className='btn'>Sign up</button>
+          <p>
+            You don't have an account yet.{' '}
+            <Link href='/auth/login'>Login</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
