b0c9fe498f23ed0d43a879afa26827a63c59d197
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-01-21 22:25:01 +0800

    create dashboard component

--- src/pages/admin/dashboard/index.js
+++ src/pages/admin/dashboard/index.js
@@ -1,5 +1,46 @@
+import Layout from '../../../components/layout';
+
+import styles from './dashboard.module.css';
+
 function Course() {
-  return <h1>Course</h1>;
+  return (
+    <Layout>
+      <div id='dashboard'>
+        <div className={`container ${styles.dashboard}`}>
+          <h3>Videos</h3>
+          <div className={`flex ${styles.options}`}>
+            <span>selected</span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+          <table className={styles.videos}>
+            <thead>
+              <tr>
+                <th>
+                  <input type='checkbox' name='selectAll' id='selectAll' />
+                </th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Visibility</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <input type="checkbox" name="" id="" />
+                </td>
+                <td>Random Video</td>
+                <td>Beginner</td>
+                <td>Public</td>
+                <td>MM-DD-YYYY HH:MM:SS</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default Course;
