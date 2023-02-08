0f2ce745386f5a0f6c224485abb5a6bcbd75fb36
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-02-08 22:41:03 +0800

    list all available courses

--- src/pages/admin/dashboard/index.js
+++ src/pages/admin/dashboard/index.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
 import Layout from '../../../components/layout';
 
 import styles from './dashboard.module.css';
 
 function Course() {
+
+  const [courses, setCourses] = useState([]);
+
+  const getAllCourses = async () => {
+    try {
+      const availableCourses =  await axios.get("http://localhost:8000/api/courses");
+      return availableCourses.data;
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getAllCourses().then(response => setCourses(response));
+  }, []) 
+
+  console.log(courses);
+
   return (
     <Layout>
       <div id='dashboard'>
@@ -21,20 +43,27 @@ function Course() {
                 </th>
                 <th>Title</th>
                 <th>Category</th>
-                <th>Visibility</th>
                 <th>Date</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  <input type="checkbox" name="" id="" />
-                </td>
-                <td>Random Video</td>
-                <td>Beginner</td>
-                <td>Public</td>
-                <td>MM-DD-YYYY HH:MM:SS</td>
-              </tr>
+              {
+                courses.map((course) => {
+
+                  const { title, category, createdAt } = course;
+
+                 return (
+                   <tr>
+                     <td>
+                       <input type="checkbox" name="" id="" />
+                     </td>
+                     <td>{title}</td>
+                     <td>{category}</td>
+                     <td>{createdAt}</td>
+                   </tr>
+                 );
+                })
+              }
             </tbody>
           </table>
         </div>
