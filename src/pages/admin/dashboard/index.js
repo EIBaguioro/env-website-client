66fb0d6fbd4d9cc3ee84bda52a5e08ab98cf04e9
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-02-09 14:47:02 +0800

    make component handle changes when inputs are triggered

--- src/pages/admin/dashboard/index.js
+++ src/pages/admin/dashboard/index.js
@@ -1,69 +1,94 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-import Layout from '../../../components/layout';
+import Layout from "../../../components/layout";
 
-import styles from './dashboard.module.css';
+import styles from "./dashboard.module.css";
 
 function Course() {
-
   const [courses, setCourses] = useState([]);
+  const [selectedCourses, setSelectedCourses] = useState([]);
+
+  const handleChange = (event) => {
+    const item = event.target;
+
+    const isItemChecked = item.checked;
+    const itemId = item.dataset.id;
+    const itemName = item.name;
+
+    if (isItemChecked) {
+      return setSelectedCourses((currentData) => [
+        ...currentData,
+        { id: itemId, name: itemName },
+      ]);
+    }
+
+    const filteredItems = selectedCourses.filter((existingItem) => {
+      return existingItem.id !== itemId;
+    });
+
+    setSelectedCourses([...filteredItems]);
+  };
 
   const getAllCourses = async () => {
     try {
-      const availableCourses =  await axios.get("http://localhost:8000/api/courses");
+      const availableCourses = await axios.get(
+        "http://localhost:8000/api/courses"
+      );
       return availableCourses.data;
-    } catch(error) {
+    } catch (error) {
       console.error(error);
     }
-  }
+  };
+
+  console.log(selectedCourses);
 
   useEffect(() => {
-    getAllCourses().then(response => setCourses(response));
-  }, []) 
-
-  console.log(courses);
+    getAllCourses().then((response) => setCourses(response));
+  }, []);
 
   return (
     <Layout>
-      <div id='dashboard'>
+      <div id="dashboard">
         <div className={`container ${styles.dashboard}`}>
           <h3>Videos</h3>
-          <div className={`flex ${styles.options}`}>
-            <span>selected</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </div>
+          {selectedCourses.length > 0 && (
+            <div className={`flex ${styles.options}`}>
+              <span>{selectedCourses.length} selected</span>
+              {selectedCourses.length === 1 && <button>Edit</button>}
+              <button>Delete</button>
+            </div>
+          )}
           <table className={styles.videos}>
             <thead>
               <tr>
-                <th>
-                  <input type='checkbox' name='selectAll' id='selectAll' />
-                </th>
+                <th> </th>
                 <th>Title</th>
                 <th>Category</th>
                 <th>Date</th>
               </tr>
             </thead>
             <tbody>
-              {
-                courses.map((course) => {
+              {courses.map((course) => {
+                const { id, title, category, createdAt } = course;
 
-                  const { title, category, createdAt } = course;
-
-                 return (
-                   <tr>
-                     <td>
-                       <input type="checkbox" name="" id="" />
-                     </td>
-                     <td>{title}</td>
-                     <td>{category}</td>
-                     <td>{createdAt}</td>
-                   </tr>
-                 );
-                })
-              }
+                return (
+                  <tr key={id}>
+                    <td>
+                      <input
+                        type="checkbox"
+                        name={title}
+                        onChange={handleChange}
+                        data-id={id}
+                      />
+                    </td>
+                    <td>{title}</td>
+                    <td>{category}</td>
+                    <td>{createdAt}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
