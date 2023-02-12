a662999a65d09f3ee531c14b48018a9dc5da15c5
Emil Baguioro I <emilbbaguioroi@gmail.com>
2023-02-12 21:59:09 +0800

    make component to receive param from dynamic route

--- src/pages/admin/course/edit/[id].js
+++ src/pages/admin/course/edit/[id].js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import CourseForm from "@/components/course-form";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 function Course() {
+
   const [courseToEdit, setCourseToEdit] = useState({});
+
+  const router = useRouter();
+  const { id } = router.query;
+
   const getCourse = async () => {
-    return await axios.get("http://localhost:8000/api/courses/34");
+    return await axios.get(`http://localhost:8000/api/courses/${id}`);
   };
 
   useEffect(() => {
